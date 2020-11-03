d3a85ed60f937c08f79aa91d150a49d5faa16876
John McGovern <73836661+John-McGovern@users.noreply.github.com>
2020-11-03 11:41:09 +0000

    03/11/20 11:40
    hash function not currently working need to use // operator to check first digits

--- hash_creator.py
+++ hash_creator.py
@@ -1,7 +1,11 @@
 from hashlib import sha256
 
 def create_hash(data):
-    hash = sha256(data.encode()).hexdigest()
+    proof = str(0)
+    hash = 11
+    while hash[0] != 0 and hash[1] != 0:
+        hash = sha256(data.encode()).hexdigest() + sha256(proof).hexdigest()
+        proof+=1 
     return hash
 
 

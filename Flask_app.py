d1790a99aa0c1d47e1aebdaa9a2b99f660a1f7df
John McGovern <73836661+John-McGovern@users.noreply.github.com>
2020-11-11 17:57:34 +0000

    11/11/202017:57
    Can get data out of forms, about to create if statement

--- Flask_app.py
+++ Flask_app.py
@@ -1,10 +1,18 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from blockchain_basis import Blockchain
 
+new = Blockchain("new")
 
 
 blockchain_app = Flask(__name__)
 
-@blockchain_app.route("/")
+@blockchain_app.route("/", methods=["GET", "POST"])
 def blockchain_page():
-    return render_template("Blockchain_template.html", app_name = "Blockchain")
+    data_to_add = request.form["block_add"]
+    if_block_made = new.create_node(data_to_add)
+    return render_template("Blockchain_template.html", app_name = "Blockchain", if_block_made = if_block_made)
+
+@blockchain_app.route("/add_block_output.html")
+def add_block_output():
+    return(render_template("add_block_output.html", block_added = "True", new_block_hash = "Test"))

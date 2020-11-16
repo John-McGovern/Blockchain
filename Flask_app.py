9579af8a6c8b1b90cdc0b93191897c054bc9509d
John McGovern <73836661+John-McGovern@users.noreply.github.com>
2020-11-16 20:23:41 +0000

    16/11/20 20:23
    Updated the forms

--- Flask_app.py
+++ Flask_app.py
@@ -3,11 +3,13 @@ from flask import Flask, render_template, request, redirect
 from blockchain_basis import Blockchain
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import DataRequired
 #import web_blockchain_initalisation
 import json
 
 users = ["visibletitle", "kittdermis", "snowflakeshiver"]
 
+users_dict = {"visibletitle":"visibletitle", "kittdermis": "kittdermis", "snowflakeshiver": "snowflakeshiver"} 
 
 online = Blockchain("online", users)
 
@@ -15,17 +17,22 @@ online = Blockchain("online", users)
 blockchain_app = Flask(__name__)
 blockchain_app.config["SECRET_KEY"] = "my_secret"
 
-
+class CreateBlockForm(FlaskForm):
+    data = StringField("Enter Data:", validators = [DataRequired()])
+    sender = SelectField("Sender:", choices = users_dict, validators = [DataRequired()])
+    receiver = SelectField("Receiver:", choices = users_dict, validators = [DataRequired()])
+    submit = SubmitField("submit")
 
 class UpdateForm(FlaskForm):
-    comment = StringField("comment")
-    select = SelectField("select", choices = {"visibletitle":"visibletitle", "kittdermis": "kittdermis", "snowflakeshiver": "snowflakeshiver"})
+    comment = StringField("comment", validators = [DataRequired()])
+    select = SelectField("select", choices = users_dict, validators = [DataRequired()])
     submit = SubmitField("submit")
 
 
-
+"""
 @blockchain_app.route("/", methods = ["GET","POST"])
 def blockchain_page():
+    create_block_form = CreateBlockForm()
     update_form = UpdateForm()
     created_block = None
     if len(request.form) > 0:
@@ -35,7 +42,23 @@ def blockchain_page():
       created_block = online.create_node(data, form_from, to)
     with open("online.json") as readable:
         message = json.load(readable)
-    return render_template("Blockchain_template.html", message = message, app_name = "Blockchain", created_block = created_block, users = users, template_form = update_form)
+    return render_template("Blockchain_template.html", message = message, app_name = "Blockchain", created_block = created_block, users = users, update_form = update_form, create_block_form = create_block_form )
+"""
+
+@blockchain_app.route("/", methods = ["GET","POST"])
+def blockchain_page():
+    create_block_form = CreateBlockForm()
+    update_form = UpdateForm()
+    created_block = None
+    
+    form_from = create_block_form.sender.data
+    to = create_block_form.receiver.data
+    data = create_block_form.data.data
+    if data:
+        created_block = online.create_node(data, form_from, to)
+    with open("online.json") as readable:
+        message = json.load(readable)
+    return render_template("Blockchain_template.html", message = message, app_name = "Blockchain", created_block = created_block, users = users, update_form = update_form, create_block_form = create_block_form )
 
 @blockchain_app.route("/update", methods = ["GET","POST"])
 def update_users_chain():

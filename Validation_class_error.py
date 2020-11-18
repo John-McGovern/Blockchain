01fc887015360f64cccd6be685b1cf49ba5b506b
John McGovern <73836661+John-McGovern@users.noreply.github.com>
2020-11-18 20:23:09 +0000

    20.21 18/11/20
    Finished cleaning the project and writing descriptions of the files. Going to stop the blocks being sent to the same user as who has sent them.

--- Validation_class_error.py
+++ Validation_class_error.py
@@ -1,6 +1,6 @@
 """
-class Error():
-    pass
+This file holds a class definiton for a hash validation error, which will be raised when a block that's hash doesnt meet the
+proof of work standard set out by the blockchain function (e.g . not starting with "00"), attemptes to be added to the chain.
 """
 
 
